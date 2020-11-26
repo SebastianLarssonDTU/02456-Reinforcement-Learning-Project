3485244f6929a4d7661734cc277ae1a69379f8fb
Henriette <h.henriette@hotmail.com>
2020-11-26 13:49:03 +0100

    change eval log

--- ppo.py
+++ ppo.py
@@ -91,7 +91,7 @@ class PPO():
             #add header
             header = ""
             for i in range(self.num_envs):
-                header += "env_{},".format(i)
+                header += "env_{}(mean),env_{}(var)".format(i,i)
             header += "avg\n"
             add_to_data_file(header, self.file_name+'_EVAL' + '.csv')
 
@@ -137,7 +137,7 @@ class PPO():
                 if self.print_output:
                     print("Evaluation done with avg score of {:10f}".format(total_reward))                
                 for key in sorted(all_episode_rewards.keys()):
-                    add_to_data_file("{:10f},".format(np.mean(all_episode_rewards[key])), self.file_name+'_EVAL' + '.csv')
+                    add_to_data_file("{:10f},{:10f}".format(np.mean(all_episode_rewards[key]), np.var(all_episode_rewards[key])), self.file_name+'_EVAL' + '.csv')
                 add_to_data_file("{:10f}\n".format(total_reward), self.file_name+'_EVAL' + '.csv')
         #end while loop
 
